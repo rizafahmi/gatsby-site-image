947e0cbab7e2c141762841248bb02fea0287eb34
Riza Fahmi <rizafahmi@gmail.com>
2018-11-19 11:03:18 +0700

    Add tachyons

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,18 @@ import Layout from '../components/layout';
 
 const IndexPage = ({ data }) => (
   <Layout>
-    {data.allImageSharp.edges.map(({ node }) => (
-      <div>
-        <Img fluid={node.fluid} />
-        <h3>{node.fluid.originalName}</h3>
-      </div>
-    ))}
+    <main>
+      <article>
+        <h2 className="f3 fw4 pa3 mv0">Images</h2>
+        {data.allImageSharp.edges.map(({ node }) => (
+          <div className="fl w-100 w-50-m w-50-l pa2">
+            <a href="#" className="db link dim tc">
+              <Img className="w-100 db outline black-10" fluid={node.fluid} />
+            </a>
+          </div>
+        ))}
+      </article>
+    </main>
   </Layout>
 );
 
